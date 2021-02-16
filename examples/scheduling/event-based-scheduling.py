64f590246303cf31fe3b472d81c09fde6b899b2f
olivier.abz <olivier.abz@gmail.com>
2021-02-16 17:00:22 +0100

    clean event-based-scheduling example script

--- examples/scheduling/event-based-scheduling.py
+++ examples/scheduling/event-based-scheduling.py
@@ -38,83 +38,6 @@ from typing import Optional
 from skdecide import DiscreteDistribution
 
 
-class MyExampleRCPSPDomain_fixed_initial_resource_levels(SingleModeRCPSP):
-
-    def _get_objectives(self) -> List[SchedulingObjectiveEnum]:
-        return [SchedulingObjectiveEnum.MAKESPAN]
-
-    def __init__(self):
-        self.initialize_domain()
-
-    def _get_max_horizon(self) -> int:
-        return 50
-
-    def _get_successors(self) -> Dict[int, List[int]]:
-        return {1: [2,3], 2:[4], 3:[5], 4:[5], 5:[]}
-
-    def _get_tasks_ids(self) -> Union[Set[int], Dict[int, Any], List[int]]:
-        return set([1,2,3,4,5])
-
-    def _get_tasks_mode(self) -> Dict[int, ModeConsumption]:
-        return {
-            1: ConstantModeConsumption({'r1': 0, 'r2': 0}),
-            2: ConstantModeConsumption({'r1': 1, 'r2': 1}),
-            3: ConstantModeConsumption({'r1': 1, 'r2': 0}),
-            4: ConstantModeConsumption({'r1': 2, 'r2': 1}),
-            5: ConstantModeConsumption({'r1': 0, 'r2': 0})
-        }
-
-    def _get_resource_types_names(self) -> List[str]:
-        return ['r1', 'r2']
-
-    def _get_task_duration(self, task: int, mode: Optional[int] = 1, progress_from: Optional[float] = 0.) -> int:
-        all_durations = {1: 0, 2: 5, 3: 6, 4: 4, 5: 0}
-        return all_durations[task]
-
-    def _get_original_fixed_quantity_resource(self, resource: str, **kwargs) -> int:
-        all_resource_quantities = {'r1': 4, 'r2': 3}
-        return all_resource_quantities[resource]
-
-    def _get_next_resource_change_time_distribution(self, res: str, currenttime: int, previousresourcehangetime: Optional[int]):
-        dist_vals = []
-        for t in range(currenttime, self.get_max_horizon()):
-            time_without_changes = t - previousresourcehangetime
-            # p = expon.cdf(time_without_changes, scale=3)
-            p = planck.cdf(time_without_changes, lambda_=0.5)
-            if p > 0.99:
-                p = 1.
-            dist_vals.append((t, p))
-            if p == 1.:
-                break
-        return DiscreteDistribution(dist_vals)
-
-    def _get_next_resource_change_delta_distribution(self, res: str, change_time: int, previous_resource_event:(int, int)):
-        if previous_resource_event is None:
-            dist_vals = [(-1, 0.5), (1, 0.5)]
-        else:
-            previous_resource_event_delta = previous_resource_event[1]
-            if previous_resource_event_delta < 0:
-                dist_vals = [(previous_resource_event_delta - 1, 0.2),
-                             (previous_resource_event_delta + 1, 0.8)]
-            else:
-                dist_vals = [(previous_resource_event_delta - 1, 0.8),
-                             (previous_resource_event_delta + 1, 0.2)]
-        return DiscreteDistribution(dist_vals)
-
-    def _get_quantity_resource(self, res: str, time: int, next_resource_event: (int, int)):
-        next_resource_event_t = next_resource_event[0]
-        next_resource_event_delta = next_resource_event[1]
-        if next_resource_event_t <= time:
-            # Event happening before time -> need to account for the change
-            val = max(0, self._get_quantity_resource(res, next_resource_event_t-1) + next_resource_event_delta)
-        else:
-            val = self._get_quantity_resource(res, next_resource_event_t-1)
-
-        return val
-
-
-
-
 class D(UncertainSchedulingDomain,
                       SingleMode,
                       DeterministicTaskDuration,
@@ -133,6 +56,7 @@ class D(UncertainSchedulingDomain,
                       WithoutResourceCosts
                       ):
     pass
+
 
 class MyExampleRCPSPDomain_varying_initial_resource_levels(D):
 
@@ -228,30 +152,34 @@ class MyExampleRCPSPDomain_varying_initial_resource_levels(D):
         return val
 
 
+
+
+class D(UncertainSchedulingDomain,
+                      SingleMode,
+                      DeterministicTaskDuration,
+                      DeterministicTaskProgress,
+                      WithoutResourceUnit,
+                      WithoutPreallocations,
+                      WithoutTimeLag,
+                      WithoutTimeWindow,
+                      WithoutResourceSkills,
+                      WithoutResourceAvailabilityChange,
+                      WithoutConditionalTasks,
+                      RenewableOnly,
+                      ConstantResourceConsumption,  # problem with unimplemented classes with this
+                      WithoutPreemptivity,  # problem with unimplemented classes with this
+                      WithoutModeCosts,
+                      WithoutResourceCosts
+                      ):
+    pass
+
+
 def run_example():
 
     domain = MyExampleRCPSPDomain_varying_initial_resource_levels()
 
     state = domain.get_initial_state()
-    print("Initial state : ", state)
-    # actions = domain.get_applicable_actions(state)
-    # print([str(action) for action in actions.get_elements()])
-    # action = actions.get_elements()[0]
-    # new_state = domain.get_next_state(state, action)
-    # print("New state ", new_state)
-    # actions = domain.get_applicable_actions(new_state)
-    # print("New actions : ", [str(action) for action in actions.get_elements()])
-    # action = actions.get_elements()[0]
-    # print(action)
-    # new_state = domain.get_next_state(new_state, action)
-    # print("New state :", new_state)
-    # print('_is_terminal: ', domain._is_terminal(state))
-    # ONLY KEEP LINE BELOW FOR SIMPLE ROLLOUT
     solver = None
-    # UNCOMMENT BELOW TO USE ASTAR
-    # domain.set_inplace_environment(False)
-    # solver = lazy_astar.LazyAstar(from_state=state, heuristic=None, verbose=True)
-    # solver.solve(domain_factory=lambda: domain)
     states, actions, values = rollout_episode(domain=domain,
                                               max_steps=1000,
                                               solver=solver,
